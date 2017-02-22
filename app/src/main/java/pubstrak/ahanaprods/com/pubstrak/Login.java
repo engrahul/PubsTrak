ffd2e7abde8033deb00924415e9859c7b3a35fc0
engrahul <engrahul@gmail.com>
2017-02-22 12:30:57 +0530

    ok

--- app/src/main/java/pubstrak/ahanaprods/com/pubstrak/Login.java
+++ app/src/main/java/pubstrak/ahanaprods/com/pubstrak/Login.java
@@ -1,33 +1,113 @@
 package pubstrak.ahanaprods.com.pubstrak;
 
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    //declarations
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btLogin;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+    //TODO: remember password
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //initialize the value in controls
+        etEmail = (EditText) findViewById(R.id.editEmail);
+        etPassword = (EditText) findViewById(R.id.editPwd);
+        btLogin = (Button) findViewById(R.id.btLogin);
+
+        //initialize Progress Dialog
+        progressDialog = new ProgressDialog(this);
+        //initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //check if user already logged in
+        if(firebaseAuth.getCurrentUser() != null){
+            //user is already logged in, go to main activity
+            finish();   //finish the current activity
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        //function for responding to clicks
+        btLogin.setOnClickListener(this);
     }
 
-    //call ValidateCrentials function on click of submit button
-    public void ValidateCredentials(View view){
-        Log.v("TAG", "In ValidateCrentials...");
-        //TODO: Authenticate using username & password
-        //TODO: Check if user is Active
-        //TODO: Check if user has admin rights
-        Intent intent = new Intent(this, MainActivity.class);
-        Log.v("TAG", "Starting Main Screen...");
-        startActivity(intent);
-        //prevent to come back to this screen by killing it
-        Log.v("TAG", "Calling finish()...");
-        finish();
+    // [START] function userLogin
+    private void userLogin(){
+        String strEmail = etEmail.getText().toString().trim();
+        String strPassword = etPassword.getText().toString().trim();
+
+        //check if email and password are not blank
+        if(TextUtils.isEmpty(strEmail)){
+            Toast.makeText(this, "Please enter a valid email.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(strPassword)){
+            Toast.makeText(this, "Please enter a valid password.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //if email and pwd are valid, show progress dialog
+        progressDialog.setMessage("Login in progress...");
+        progressDialog.show();
+
+        //call firbase to validate credentials
+        Log.d("TAG", "Before auth");
+        firebaseAuth.signInWithEmailAndPassword(strEmail, strPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            //on successful login, call the main page
+                            finish();   //finish the current activity
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            //login not successful
+                            Toast.makeText(getApplicationContext(), "Login not successful. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+    // [END] function userLogin
+
+    @Override
+    public void onClick(View v) {
+        //check if button is clicked
+        if(v == btLogin){
+            //call custom method
+            userLogin();
+            //Toast.makeText(this, "Login not successful. Please try again.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     //TODO: version label at the bottom
     //TODO: implement 'Forgot Password'
--- app/src/main/java/pubstrak/ahanaprods/com/pubstrak/MainActivity.java
+++ app/src/main/java/pubstrak/ahanaprods/com/pubstrak/MainActivity.java
@@ -1,13 +1,53 @@
 package pubstrak.ahanaprods.com.pubstrak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //declare
+    private FirebaseAuth firebaseAuth;
+
+    private TextView tvUser;
+    private Button btLogoff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        tvUser = (TextView) findViewById(R.id.tvUser);
+        btLogoff = (Button) findViewById(R.id.btLogOff);
+
+        //check if user is not logged
+
+        //get current user
+        FirebaseUser fbUser = firebaseAuth.getCurrentUser();
+        //set label to email of user logged in
+        tvUser.setText(fbUser.getEmail() + " logged");
+
+        //attach the listner
+        btLogoff.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        //check id Logoff button is clicked
+        if(v == btLogoff){
+            //logoff the user
+            finish();   //finish the current activity
+            firebaseAuth.signOut();
+            startActivity(new Intent(getApplicationContext(), Login.class));
+        }
+    }
+
 }
